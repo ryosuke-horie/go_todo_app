8343e9850926cacfea82949291b42802a0594229
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-11-21 07:44:33 +0900

    Runの説明を追記

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 )
 
 // メインプロセスを実装するときに資料されるrun関数のパターン
+// context.Context方の値から終了通知を待機するrun関数
+// チャネルからの終了通知があると次の順序でrun関数が終了する
+// 1. 「<-ctx.Done()」の津g期の行の*http.Server.Shutdownメソッドを呼び出す
+// 2. 別goroutineで実行していた*http.Server.ListenAndServeメソッドが終了する
+// 3. 別goroutineで実行されていた無名関数が終了する
+// 4. run関数の最後で待機していたerrgroup.Group.Waitメソッドが終了する
+// 5.
 func run(ctx context.Context) error {
 	s := http.Server{
 		Addr: ":18080",
