873671f91d9b0eb1e80fb6d847eca56e94de45ac
ryosuke-horie <r-horie@mamiya-its.co.jp>
2024-11-21 01:00:40 +0900

    外部から終了を指示された時サーバーを終了するrun関数を実装

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ryosuke-horie/go_todo_app
 
 go 1.23.2
+
+require golang.org/x/sync v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,31 +1,48 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
-// 以下の問題点を持っているコード
-// 1. テスト完了後に終了する方法がない
-// 2. 出力が検証しにくい
-// 3. 異常時にos.Exitが呼ばれる
-// 4. ポート番号が固定されるためテストの起動に失敗する可能性がある
-
-func main() {
-	// ListenAndServeは第一引数のアドレス文字列と第二引数のハンドラでHTTPサーバを起動する
-	err := http.ListenAndServe(
-		":18080",
-		// 第二引数に単一のハンドラを渡すことで、全てのリクエストに対して同じ処理を行う
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// パスを使ってレスポンスを返す
+// メインプロセスを実装するときに資料されるrun関数のパターン
+func run(ctx context.Context) error {
+	s := http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "hello, %s", r.URL.Path[1:])
 		}),
-	)
-
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
 
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別のgoroutineでサーバを起動する
+	eg.Go(func() error {
+		// http.ErrServerClosedは
+		// http.Server.shutdownが正常に終了したことを示すため以上ではない
+		if err := s.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+
+		return nil
+	})
+
+	// チャンネルからの通知（終了通知）を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別のgoroutineの終了を待つ
+	return eg.Wait()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		fmt.Fprintf("failed to terminate server: %v", err)
+	}
 }
