7f17c1e72b1e22e8217b4e254260262621228a31
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-21 15:35:25 +0900

    run関数のテストコードを実装

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func run(ctx context.Context) error {
 	s := http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "hello, %s", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 		}),
 	}
 
