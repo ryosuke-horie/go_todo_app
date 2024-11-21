ea972cdd1a509698ce362e5f6c826053112d120c
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-22 08:11:05 +0900

    型エラー解消

--- store/store.go
+++ store/store.go
@@ -7,29 +7,36 @@ import (
 )
 
 var (
-	Tasks = &TaskStore{Tasks: map[int]*entity.Task{}}
+	Tasks = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
 
 	ErrNotFound = errors.New("not found")
 )
 
 type TaskStore struct {
-	// 動作確認用の仮実装のためあえてエクスポート
+	// 動作確認用の仮実装なのであえてexportしている。
 	LastID entity.TaskID
-	Tasks  map[int]*entity.Task
+	Tasks  map[entity.TaskID]*entity.Task
 }
 
-func (ts *TaskStore) Add(t *entity.Task) (int, error) {
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
 	ts.LastID++
 	t.ID = ts.LastID
 	ts.Tasks[t.ID] = t
 	return t.ID, nil
 }
 
-func (ts *TaskStore) All() entity.Task {
+func (ts *TaskStore) Get(id entity.TaskID) (*entity.Task, error) {
+	if ts, ok := ts.Tasks[id]; ok {
+		return ts, nil
+	}
+	return nil, ErrNotFound
+}
+
+// All はソート済みのタスク一覧を返す
+func (ts *TaskStore) All() entity.Tasks {
 	tasks := make([]*entity.Task, len(ts.Tasks))
 	for i, t := range ts.Tasks {
 		tasks[i-1] = t
 	}
-
 	return tasks
 }
