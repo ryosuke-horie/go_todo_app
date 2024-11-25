c97791f9f22e0eb11bc71e4906e628e5ca095eb2
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-25 15:16:06 +0900

    Shema修正

--- _tools/mysql/schema.sql
+++ _tools/mysql/schema.sql
@@ -1,14 +1,14 @@
--- CREATE TABLE `user`
--- (
---     `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
---     `name`     varchar(20) NOT NULL COMMENT 'ユーザー名',
---     `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
---     `role`     VARCHAR(80) NOT NULL COMMENT 'ロール',
---     `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
---     `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
---     PRIMARY KEY (`id`),
---     UNIQUE KEY `uix_name` (`name`) USING BTREE
--- ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
+CREATE TABLE `user`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーの識別子',
+    `name`     varchar(20) NOT NULL COMMENT 'ユーザー名',
+    `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
+    `role`     VARCHAR(80) NOT NULL COMMENT 'ロール',
+    `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
+    `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uix_name` (`name`) USING BTREE
+) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザー';
 
 CREATE TABLE `task`
 (
@@ -17,5 +17,5 @@ CREATE TABLE `task`
     `status`   VARCHAR(20)  NOT NULL COMMENT 'タスクの状態',
     `created`  DATETIME(6) NOT NULL COMMENT 'レコード作成日時',
     `modified` DATETIME(6) NOT NULL COMMENT 'レコード修正日時',
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='タスク';
