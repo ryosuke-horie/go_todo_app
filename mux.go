0043ab3410162a95ac4b5154596f97084ba811b4
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-25 18:53:58 +0900

    mux関数及びrun関数からRDBMSを利用する処理に差し替え

--- mux.go
+++ mux.go
@@ -1,32 +1,38 @@
 package main
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-playground/validator/v10"
+	"github.com/ryosuke-horie/go_todo_app/clock"
+	"github.com/ryosuke-horie/go_todo_app/config"
 	"github.com/ryosuke-horie/go_todo_app/handler"
 	"github.com/ryosuke-horie/go_todo_app/store"
 )
 
-// HTTPハンドラーの定義
-func NewMux() http.Handler {
-	// mux := http.NewServeMux()
+func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), error) {
 	mux := chi.NewRouter()
-
-	// HTTPサーバーが稼働中であることを確認するためのヘルスチェックエンドポイント
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		// 静的解析を通すために明示的に戻り値を捨てる
-		_, _ = w.Write([]byte(`{"status":"ok"}`))
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
 
 	v := validator.New()
-	at := &handler.AddTask{Store: store.Tasks, Validator: v}
-	mux.Post("/tasks", at.ServeHTTP)
 
-	lt := &handler.ListTask{Store: store.Tasks}
+	db, cleanup, err := store.New(ctx, cfg)
+	if err != nil {
+		return nil, cleanup, err
+	}
+
+	r := store.Repository{Clocker: clock.RealClocker{}}
+
+	at := &handler.AddTask{DB: db, Repo: &r, Validator: v}
+
+	mux.Post("/tasks", at.ServeHTTP)
+	lt := &handler.ListTask{DB: db, Repo: &r}
 	mux.Get("/tasks", lt.ServeHTTP)
 
-	return mux
+	return mux, cleanup, nil
 }
