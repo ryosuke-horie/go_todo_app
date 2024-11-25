83eb0d9c7403ed6610ea761ca6aba01f83164c83
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-25 19:08:55 +0900

    mysqlドライバの読み込みを追加

--- store/repository.go
+++ store/repository.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/ryosuke-horie/go_todo_app/clock"
 	"github.com/ryosuke-horie/go_todo_app/config"
