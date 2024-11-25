af351ba0d0f45a06c14f8b3b53eec1a39b34e952
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-25 19:14:21 +0900

    DBに接続できない問題を解消

--- store/repository.go
+++ store/repository.go
@@ -13,42 +13,41 @@ import (
 )
 
 func New(ctx context.Context, cfg *config.Config) (*sqlx.DB, func(), error) {
-	// sqlx.Connectを使うと内部でpingする
+	// sqlx.Connectを使うと内部でpingする。
 	db, err := sql.Open("mysql",
 		fmt.Sprintf(
 			"%s:%s@tcp(%s:%d)/%s?parseTime=true",
-			cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.Port, cfg.DBName,
+			cfg.DBUser, cfg.DBPassword,
+			cfg.DBHost, cfg.DBPort,
+			cfg.DBName,
 		),
 	)
 	if err != nil {
-		return nil, nil, err
+		return nil, func() {}, err
 	}
-
-	// Openは実際に接続テストが行われない
+	// Openは実際に接続テストが行われない。
 	ctx, cancel := context.WithTimeout(ctx, 2*time.Second)
 	defer cancel()
 
 	if err := db.PingContext(ctx); err != nil {
-		return nil, func() { _ = db.Close() }, nil
+		return nil, func() { _ = db.Close() }, err
 	}
-
 	xdb := sqlx.NewDb(db, "mysql")
 	return xdb, func() { _ = db.Close() }, nil
 }
 
-// ↓DB操作を抽象化するためのインターフェースを定義する
+type Repository struct {
+	Clocker clock.Clocker
+}
 
-// トランザクションを開始するためのメソッドを定義
 type Beginner interface {
 	BeginTx(ctx context.Context, opts *sql.TxOptions) (*sql.Tx, error)
 }
 
-// コンテキストを利用してクエリを準備するメソッドを定義
 type Preparer interface {
 	PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error)
 }
 
-// クエリを実行するためのメソッドを定義
 type Execer interface {
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
 	NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error)
@@ -62,20 +61,11 @@ type Queryer interface {
 	SelectContext(ctx context.Context, dest interface{}, query string, args ...any) error
 }
 
-// インターフェースがsqlx.DB及びsqlx.Tx型に正しく実装されているか確認
 var (
-	// インターフェースが期待通りに宣言されているか確認
+	// インターフェイスが期待通りに宣言されているか確認
 	_ Beginner = (*sqlx.DB)(nil)
 	_ Preparer = (*sqlx.DB)(nil)
 	_ Queryer  = (*sqlx.DB)(nil)
 	_ Execer   = (*sqlx.DB)(nil)
 	_ Execer   = (*sqlx.Tx)(nil)
 )
-
-// RDBMSに対する永続化操作をRepository型のメソッドに定義する
-// 同じ型のメソッドとして実装することで
-// １．複数のテーブルを1つの型のメソッドで操作できる
-// ２．DIを利用する場合、１つの型にまとまっていた方が取り回しがしやすい
-type Repository struct {
-	Clocker clock.Clocker
-}
