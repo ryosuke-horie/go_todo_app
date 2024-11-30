a39ed752547c03c39da6b060c4a360594c902301
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-11-30 14:55:20 +0900

    ユーザー登録用のエンドポイントを実装

--- mux.go
+++ mux.go
@@ -35,5 +35,11 @@ func NewMux(ctx context.Context, cfg *config.Config) (http.Handler, func(), erro
 	lt := &handler.ListTask{Service: &service.ListTasks{DB: db, Repo: &r}}
 	mux.Get("/tasks", lt.ServeHTTP)
 
+	ru := &handler.RegisterUser{
+		Service:   &service.RegisterUser{DB: db, Repo: &r},
+		Validator: v,
+	}
+	mux.Post("/register", ru.ServeHTTP)
+
 	return mux, cleanup, nil
 }
