b840fdcd292d7858ac5c8d8412b9126ff9bbbeb4
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-12-07 19:29:24 +0900

    公開鍵と秘密鍵を埋め込み

---
+++ auth/jwt_test.go
@@ -0,0 +1,18 @@
+package auth
+
+import (
+	"bytes"
+	"testing"
+)
+
+// 公開鍵及び秘密鍵が埋め込まれているか確認する
+func TestEmbed(t *testing.T) {
+	want := []byte("-----BEGIN PUBLIC KEY-----")
+	if !bytes.Contains(rawPubKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPubKey)
+	}
+	want = []byte("-----BEGIN PRIVATE KEY-----")
+	if !bytes.Contains(rawPrivKey, want) {
+		t.Errorf("want %s, but got %s", want, rawPrivKey)
+	}
+}
