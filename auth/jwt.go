46869a494517839ae3fc9efb029249f25ebfbe42
ryosuke-horie <ryosuke.horie37@gmail.com>
2024-12-07 20:07:13 +0900

    JWT発行用の関数を実装

--- auth/jwt.go
+++ auth/jwt.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
 	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
 	"github.com/ryosuke-horie/go_todo_app/clock"
 	"github.com/ryosuke-horie/go_todo_app/entity"
 )
@@ -60,4 +64,40 @@ func parse(rawKey []byte) (jwk.Key, error) {
 		return nil, err
 	}
 	return key, nil
+}
+
+const (
+	RoleKey     = "role"
+	UserNameKey = "user_name"
+)
+
+// 署名済みのJWTを生成する
+// 引数に渡したユーザーの値に対してJWTを発行する
+func (j *JWTer) GenerateToken(ctx context.Context, u entity.User) ([]byte, error) {
+	tok, err := jwt.NewBuilder().
+		JwtID(uuid.New().String()).
+		Issuer(`github.com/budougumi0617/go_todo_app`).
+		Subject("access_token").
+		IssuedAt(j.Clocker.Now()).
+		// redisのexpireはこれを使う。
+		// https://pkg.go.dev/github.com/go-redis/redis/v8#Client.Set
+		// clock.Durationだから Subする必要がある
+		Expiration(j.Clocker.Now().Add(30*time.Minute)).
+		Claim(RoleKey, u.Role).
+		Claim(UserNameKey, u.Name).
+		Build()
+	if err != nil {
+		return nil, fmt.Errorf("GenerateToken: failed to build token: %w", err)
+	}
+	// jwtをRedisに保存して管理
+	if err := j.Store.Save(ctx, tok.JwtID(), u.ID); err != nil {
+		return nil, err
+	}
+
+	// 秘密鍵でJWTに署名してトークン文字列を生成
+	signed, err := jwt.Sign(tok, jwt.WithKey(jwa.RS256, j.PrivateKey))
+	if err != nil {
+		return nil, err
+	}
+	return signed, nil
 }
